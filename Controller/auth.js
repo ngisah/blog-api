5973b7d1e4c2b12e0996daad1eec05e6381b370e
edwin <ngisahedwin@gmail.com>
2023-05-16 18:21:27 +0300

    registration and authentication done

--- Controller/auth.js
+++ Controller/auth.js
@@ -1,12 +1,36 @@
+import { db } from "../db";
+import bcrypt from "bcryptjs";
+
 export const register = (req, res) =>{
-    // chck existing user
+    // check existing user
 
     const q = "SELECT FROM users WHERE username=? OR email=?"
     db.query(q, [req.body.username, req.body.email], (err, res) =>{
         if(err) return res.json(err);
         if(data.length) return res.status(409).json('user already exists');
+
+        // hash the password and create user
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash =bcrypt.hashSync(req.body.passwords, salt);
+
+        const q = "INSERT INTO users (`username`, `email`, `password`) VALUES(?)"
+
+        const values = [
+            req.body.username, 
+            req.body.email,  
+            hash
+        ]
+
+        db.query(q, [values], (err, res) => {
+            if (err) return res.json(err);
+            return res.status(200).json('User has been created.');
+
+
+
     })
 
+})
 }
 
 export const login = (req, res) =>{
